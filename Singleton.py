7f3a84a9d0403b2444ec482712133c2b7d19256c
Sergey Alferov <107875421+nfgone23@users.noreply.github.com>
2026-01-03 00:56:23 +0300

    Create Singleton.py

---
+++ Singleton.py
@@ -0,0 +1,15 @@
+class Singleton:
+    _instance = None
+    
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+    
+    def __init__(self):
+        self.data = "Я синглтон"
+
+# Использование
+s1 = Singleton()
+s2 = Singleton()
+print(s1 is s2)  # True
